8ad2265b94b5d6d41e1620f5b28a97ffda5cfdcc
Douglas Chun <dugchun@aol.com>
2016-02-08 14:33:24 -0800

    second commit

--- app/controllers/challenge_controller.rb
+++ app/controllers/challenge_controller.rb
@@ -1,6 +1,9 @@
 class ChallengeController < ApplicationController
   def code
-
-    
+    render :json => {
+      name: 'Douglas Chun',
+      email: 'dugchun@aol.com',
+      website:'none',
+      github_repo_link: 'https://github.com/toffguns/zestychallenge'}.to_json    
   end
 end
